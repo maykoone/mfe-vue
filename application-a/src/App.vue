be3922b07e61c4aba4565ef4b75aa41f932c2d03
Mayko B. Oliveira <maykoone@gmail.com>
2024-02-26 23:32:00 -0300

    added webpack module federation config to consume component from application-b

--- application-a/src/App.vue
+++ application-a/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorldFromB msg="Hello from Application A"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorldFromB: () => import("application_b/HelloWorldFromB"),
   }
 }
 </script>
--- application-a/vue.config.js
+++ application-a/vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ModuleFederationPlugin } = require("webpack").container;
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  //transpileDependencies: true,
+  publicPath: 'http://localhost:8080/',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'application_a',
+        filename: 'remoteEntry.js',
+        remotes: {
+          application_b: 'application_b@http://localhost:9001/remoteEntry.js',
+        },
+        shared: require('./package.json').dependencies,
+      }),
+    ],
+  },
 })
