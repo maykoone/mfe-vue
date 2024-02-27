8b60ca1f5c07a8aec9f1960e3975f67aa262cb5b
Mayko B. Oliveira <maykoone@gmail.com>
2024-02-26 23:27:18 -0300

    added webpack module federation configuration to application b

--- application-b/vue.config.js
+++ application-b/vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ModuleFederationPlugin } = require("webpack").container;
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  //transpileDependencies: true,
+  publicPath: 'http://localhost:9001/',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'application_b',
+        filename: 'remoteEntry.js',
+        library: { type: 'var', name: 'other' },
+        exposes: {
+          './HelloWorldFromB': './src/components/HelloWorld',
+        },
+        shared: require('./package.json').dependencies,
+      }),
+    ],
+  },
+  devServer: {
+    port: 9001,
+  },
 })
